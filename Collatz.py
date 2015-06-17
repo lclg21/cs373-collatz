ad198c085abcc47d34daa94d558db936fc98781f
Luis Lopez <lclg21@utexas.edu>
2015-06-16 23:18:35 -0500

    implemented a cache to collatz.py

--- Collatz.py
+++ Collatz.py
@@ -19,6 +19,45 @@ def collatz_read (s) :
     a = s.split()
     return [int(a[0]), int(a[1])]
 
+# ----------------------
+# cache_helper_functions
+#-----------------------
+cache = []
+def addToCache(n, c):
+    """
+    n is an int
+    c is cycle length of n
+    adds the value and the corresponding cycle length to the cache
+    """
+    cache.append((n, c))
+
+def isInCache(n):
+    """
+    n is an int
+    returns true if value n is in the cache false otherwise
+    """
+    inCache = False
+    for list in cache:
+        if (list[0] == n ):
+            inCache = True
+            break
+        else:
+            inCache = False
+    return inCache
+
+def getCycleLength(n):
+    """
+    n is an int
+    returns the cycle length of n from the cache 
+    """
+    cycle = 0
+    for list in cache:
+        if (list[0] == n ):
+            cycle = list[1]
+            break
+    return cycle
+
+
 # ------------
 # collatz_eval
 # ------------
@@ -30,6 +69,7 @@ def collatz_eval (i, j) :
     return the max cycle length of the range [i, j]
     """
     
+    #swaps i and j if i > j
     maxLen = 0
     if (i > j) :
         assert i > j
@@ -37,24 +77,37 @@ def collatz_eval (i, j) :
         j = i
         i = temp
     
+    #loops through each value withing the range
     while ( i < j + 1) :
         assert i < j + 1
         n = i
-        cycleLength = 1
-        while (n != 1) :
-            assert n > 1
-            if (n % 2 == 0) : #if even
-                n = n / 2
-            else :
-                n = (3 * n) + 1 #if odd
-                
-            cycleLength += 1
-            assert cycleLength > 0
+        #checks to see if value is in cache
+        if (isInCache(n) == True) :
+            assert isInCache(n) == True
+            cycleLength = getCycleLength(n)
+            
+            if (maxLen < cycleLength) :
+                assert maxLen > 0
+                maxLen = cycleLength
+
+        #if value is not in cache, it calculates the cyclelength
+        #and adds it to the cache
+        else :
+            cycleLength = 1
+            while (n != 1) :
+                assert n > 1
+                if (n % 2 == 0) : #if even
+                    n = n / 2
+                else :
+                    n = (3 * n) + 1 #if odd
+                cycleLength += 1
+                assert cycleLength > 0
+            #adds the value and its corresponding cycle length to the cache
+            addToCache(i, cycleLength)
+            if (maxLen < cycleLength) :
+                assert maxLen > 0
+                maxLen = cycleLength
         i += 1
-        if  (maxLen < cycleLength) :
-            maxLen = cycleLength
-    
-    assert maxLen > 0
 
     return maxLen
         
